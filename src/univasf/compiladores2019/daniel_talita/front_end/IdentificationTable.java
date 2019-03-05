ece5074406fcf37c87d38babe7419c49cf3c82e1
Talita1996 <talita_mota@outlook.com>
2019-03-05 16:06:16 -0300

    Começando o analisador de contexto

---
+++ src/univasf/compiladores2019/daniel_talita/front_end/IdentificationTable.java
@@ -0,0 +1,14 @@
+package univasf.compiladores2019.daniel_talita.front_end;
+
+public class IdentificationTable {
+
+    public IdentificationTable() {
+    }
+    
+    public void enter (String id, Attribute attr) {
+    }
+    
+    public Attribute retrieve (String id) {
+        
+    }
+}
